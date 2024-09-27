6d92c28a3017a3889c2bb1a5c2d23cf4f602878e
Raul Malawene <127786126+RaulMalawene@users.noreply.github.com>
2024-09-27 20:51:14 +0200

    Add files via upload

---
+++ calculo_media/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nota1;
+            double nota2;
+
+
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine("Digite a nota1: ");
+                nota1 = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Digite a nota1: ");
+               nota2 = double.Parse(Console.ReadLine());
+
+
+            }
+
+          
+
+            
+        
+           
+
+
+        }
+    }
+}
